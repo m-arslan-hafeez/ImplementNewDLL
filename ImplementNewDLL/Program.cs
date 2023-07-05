4db9c23e901066146d7dfac5d0e591fdfe98c4e7
OpenAI <OpenAI@CHATGPT>
2023-07-05 15:27:32 +0500

    Add project files.

---
+++ ImplementNewDLL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace ImplementNewDLL
+{
+    class Program
+    {
+        [DllImport("attemptDll.dll")]
+        public static extern IntPtr Create(int x);
+        [DllImport("attemptDll.dll")]
+        public static extern int AttemptAdd(IntPtr a, int y);
+        static void Main(string[] args)
+        {
+            IntPtr a = Create(5);
+            Console.WriteLine(AttemptAdd(a, 10));
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
